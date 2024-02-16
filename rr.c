fa7d53eba6f5c2c7da65d4b9ceec1f46de8640de
Rohith Venkatesh <rvenkatesh2025@ucla.edu>
2024-02-16 01:06:43 -0800

    initial psuedocode added

--- rr.c
+++ rr.c
@@ -21,6 +21,11 @@ struct process
   TAILQ_ENTRY(process) pointers;
 
   /* Additional fields here */
+  u32 remaining_time;
+  u32 remaining_cycle_time;
+  u32 start_exec_time;
+  u32 waiting_time;
+  u32 response_time;
   /* End of "Additional fields here" */
 };
 
@@ -160,7 +165,84 @@ int main(int argc, char *argv[])
   u32 total_response_time = 0;
 
   /* Your code here */
-  
+  u32 current_time = 0;
+
+  //Initialize all other fields in process node
+  TAILQ_FOREACH(current_node, &data, pointers){
+    current_node->remaining_time = current_node->burst_time;
+    current_node->start_exec_time = -1;
+  }
+
+  struct process *current_node, *next_node;
+
+
+  for(u32 current_time = 0; ; ++current_time){
+    for (i = 0; i < size; i++)
+    {
+      if((*data)[i].arrival_time == current_time){
+        //Add to queue
+      }
+
+    }
+
+
+
+
+
+
+
+  }
+
+
+
+
+
+  //Loop and execute Round Robin
+  while(!TAILQ_EMPTY(&list))
+  {
+    current_node = TAILQ_FIRST(&list);
+    while(current_node != NULL)
+    {
+      next_node = TAILQ_NEXT(current_node, pointers);
+
+      if(current_node->start_exec_time == -1)
+      {
+        current_node->start_exec_time = current_time;
+        current_node->response_time = current_node->start_exec_time - current_node->arrival_time;
+        total_response_time += current_node->response_time;
+      }
+
+      if(current_node->remaining_time <= quantum_length)
+      {
+        current_time += current_node->remaining_time;
+        current_node->waiting_time = current_time - current_node->arrival_time - current_node->burst_time;
+        total_waiting_time += current_node->waiting_time;
+        TAILQ_REMOVE(&list, current_node, pointers);
+        free(current_node);
+      }
+      else
+      {
+        current_node->remaining_time -= quantum_length;
+        current_time += quantum time;
+      }
+
+
+      //if start_exec_time is -1
+        //current_node->start_exec_time = current_time;
+        //current_node->response_time = current_node->start_exec_time - current_node->arrival_time;
+
+      
+      //If remaining time is less than or equal to quantum time
+        //current_time += remaining time
+        //current_node->waiting_time = current_time - current_node->arrival_time - current_node->burst_time;
+        //remove from queue (cleanup)
+      //else
+        ////current_time += quantum time
+        //subtract quantum time from remaining time
+        
+      current_node = next_node;
+    }
+  }
   /* End of "Your code here" */
 
   printf("Average waiting time: %.2f\n", (float)total_waiting_time / (float)size);
